5312da304f148ad92ae5f8f3fd974f81e8d08928
Kalid-N <107488332+Mckali-2k@users.noreply.github.com>
2024-01-05 07:41:48 +0300

    Added Property Entity (#1)
    this commit contains the following
    1.  property entity that contains all the attribute of property table
    2.  propertyRepository which have a method persist that adds a property entity to our database
    3. because property table contains fk i have to assign them manually in order to create the property entity
    4. MySql is used as a database

---
+++ NurRealEstateWebApp/Entities/Property.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NurRealEstateWebApp.Entities
+{
+    public class Property
+    {
+        [Key]
+        public Guid property_id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string description { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value")]
+        public decimal price { get; set; }
+
+        [Required(ErrorMessage = "Property type is required")]
+        public string property_type { get; set; }
+
+        [Required(ErrorMessage = "Property size is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Property size must be a positive value")]
+        public decimal property_size { get; set; }
+
+        [Required(ErrorMessage = "Number of bedrooms is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Number of bedrooms must be a positive value")]
+        public int no_bedrooms { get; set; }
+
+        [Required(ErrorMessage = "Number of bathrooms is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Number of bathrooms must be a positive value")]
+        public int no_bathrooms { get; set; }
+
+        public bool has_maidsRoom { get; set; }
+
+        [Required(ErrorMessage = "Number of parking spaces is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Number of parking spaces must be a positive value")]
+        public int no_parking { get; set; }
+
+        [Required(ErrorMessage = "Listed date is required")]
+        public DateTime listed_date { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string status { get; set; }
+
+        [ForeignKey("user_id")]
+        public Guid user_id { get; set; }
+/*        public User User { get; set; }
+*/
+        [ForeignKey("agent_id")]
+        public Guid agent_id { get; set; }
+/*        public Agent Agent { get; set; }
+*/
+/*        public Address Address { get; set; }
+*/    }
+}
---
+++ NurRealEstateWebApp/Repository/PropertyRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NurRealEstateWebApp.Entities;
+using NurRealEstateWebApp.Models;
+
+namespace NurRealEstateWebApp.Repository
+{
+    public class PropertyRepository
+    {
+        private readonly NurDbContext nurDbContext;
+
+        public PropertyRepository(NurDbContext nurDbContext)
+        {
+            this.nurDbContext = nurDbContext;
+        }
+
+        public async Task<bool> Persist(Property property)
+        {
+            nurDbContext.Set<Property>().Add(property);
+            var saved = await nurDbContext.SaveChangesAsync();
+            return saved > 0;
+        }
+    }
+}
