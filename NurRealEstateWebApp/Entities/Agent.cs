0a556c7c5a013739b766b8bdd54157526b6bba06
Mckali-2k <kalid538598@gmail.com>
2024-01-17 08:31:44 +0300

    Added Address and Agent Entity
    this commit contains the following
    1. Address and Agent entity that contains all the attribute of Address and Agent table
    2. Repository for Address and Agent which have a method persist that adds an Address and Agent entity to our database

---
+++ NurRealEstateWebApp/Entities/Agent.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NurRealEstateWebApp.Entities
+{
+    public class Agent
+    {
+        [Key]
+        public Guid agent_id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string nationality { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string languages { get; set; }
+
+        [Required]
+        public DateTime experience_since { get; set; }
+
+        [ForeignKey("contact_id")]
+        public Guid contact_id { get; set; }
+/*        public Contact Contact { get; set; }
+*/
+        [ForeignKey("account_id")]
+        public Guid account_id { get; set; }
+
+/*        public Account Account { get; set; }
+*/
+/*        public ICollection<Property> Properties { get; set; }
+*/    }
+}
---
+++ NurRealEstateWebApp/Repository/AddressRepository.cs
@@ -0,0 +1,22 @@
+﻿using NurRealEstateWebApp.Entities;
+using NurRealEstateWebApp.Models;
+
+namespace NurRealEstateWebApp.Repository
+{
+    public class AddressRepository
+    {
+        private readonly NurDbContext nurDbContext;
+
+        public AddressRepository(NurDbContext _nurDbContext)
+        {
+            nurDbContext = _nurDbContext;
+        }
+
+        public async Task<bool> Persist(Address address)
+        {
+            nurDbContext.Set<Address>().Add(address);
+            var saved = await nurDbContext.SaveChangesAsync();
+            return saved > 0;
+        }
+    }
+}
